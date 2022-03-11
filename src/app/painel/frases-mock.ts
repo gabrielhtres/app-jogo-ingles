2059471b721056be93f423db55e1900075709d45
Gabriel <gabrielhtres@gmail.com>
2022-03-11 17:33:24 -0300

    Salvamento de código para usar em casa

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,12 @@
+import { Frase } from '../shared/frase.model'
+
+export const FRASES: Frase[] = [
+    { fraseEng: 'I like to learn',
+    frasePtBr: 'Eu gosto de aprender' },
+    { fraseEng: 'I watch TV',
+    frasePtBr: 'Eu assisto TV' },
+    { fraseEng: 'How are you?',
+    frasePtBr: 'Come vai você?' },
+    { fraseEng: 'I eat bread',
+    frasePtBr: 'Eu como pão' }
+]
